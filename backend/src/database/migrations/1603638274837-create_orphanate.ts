d4a7e5b7b5de8fbf008a5b5e03ac5925923e3395
mikaelgba <mikaelgba@gmail.com>
2020-10-25 12:15:23 -0300

    add backend

---
+++ backend/src/database/migrations/1603638274837-create_orphanate.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanate1603638274837 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

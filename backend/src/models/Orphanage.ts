4af13c18c6bfbb0a9a28c0e3945f92254f262ae6
mikaelgba <mikaelgba@gmail.com>
2020-11-03 14:26:21 -0200

    'up

---
+++ backend/src/models/Orphanage.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+
+/*  strictPropertyInitialization no tsconfig.ts precisa ser false*/
+/*  e também habilitar  "experimentalDecorators" e "emitDecoratorMetadata", ambos precisam ser true */
+
+@Entity('orphanages')
+export default class Orphanage {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    latitude: number;
+
+    @Column()
+    longitude: number;
+
+    @Column()
+    about: string;
+
+    @Column()
+    instructions: string;
+
+    @Column()
+    opening_hours: string;
+
+    @Column()
+    open_on_weekends: boolean;
+
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,35 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 import './database/connection';
 
 const app = express();
 app.use(express.json());
+
+app.post('/orphanages', (request, response) => {
+
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanageadd = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    })
+    orphanagesRepository.save(orphanageadd);
+
+    return response.json(orphanageadd)
+});
 
 app.listen(3333);
